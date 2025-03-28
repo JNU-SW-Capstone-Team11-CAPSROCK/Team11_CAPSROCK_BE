58ef33f4f197d299386ce50bd327a9c4bf5d1dc5
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-03-28 11:30:58 +0900

    feat: 역지오코딩 기능 추가

---
+++ src/main/java/capsrock/location/geocoding/dto/response/ReverseGeocodingResponse.java
@@ -0,0 +1,11 @@
+package capsrock.location.geocoding.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public record ReverseGeocodingResponse(
+
+) {
+
+}
