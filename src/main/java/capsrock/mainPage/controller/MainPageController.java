be4b627ba281180e4ba1abb0a58292898ae126ac
박준형 <jun@bagjunhyeong-ui-MacBookAir.local>
2025-03-29 17:35:27 +0900

    feat: MainPageController 추가

---
+++ src/main/java/capsrock/mainPage/controller/MainPageController.java
@@ -0,0 +1,31 @@
+package capsrock.mainPage.controller;
+
+import capsrock.mainPage.dto.request.MainPageRequest;
+import capsrock.mainPage.dto.response.MainPageResponse;
+import capsrock.mainPage.service.MainPageService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainPageController {
+
+    private final MainPageService mainPageService;
+
+    public MainPageController(MainPageService mainPageService) {
+        this.mainPageService = mainPageService;
+    }
+
+    @GetMapping
+    public ResponseEntity<MainPageResponse> mainPage(
+            @RequestParam("latitude") Double latitude,
+            @RequestParam("longitude") Double longitude){
+
+        MainPageRequest mainPageRequest = new MainPageRequest(latitude, longitude);
+        //return new ResponseEntity<>(mainPageService.getWeatherInfo(mainPageRequest), HttpStatus.OK);
+    }
+}
