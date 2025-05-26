a79123d27071c40f240f2bcbeaef99ab1743b50e
rdme0 <71927381+rdme0@users.noreply.github.com>
2025-05-26 18:23:49 +0900

    refactor: 위도 경도 유효성 검사 추가

--- src/main/java/capsrock/auth/dto/request/LoginRequest.java
+++ src/main/java/capsrock/auth/dto/request/LoginRequest.java
@@ -1,3 +1,21 @@
 package capsrock.auth.dto.request;
 
-public record LoginRequest(String email, String password, Double longitude, Double latitude) { }
+public record LoginRequest(String email, String password, Double longitude, Double latitude) {
+    
+    public LoginRequest {
+        validate(longitude, latitude);
+    }
+    
+    private static void validate(Double longitude, Double latitude) {
+        if (longitude == null || latitude == null) {
+            throw new IllegalArgumentException("위도 또는 경도를 입력해주세요");
+        }
+
+        if (longitude < -180 || longitude > 180) {
+            throw new IllegalArgumentException("경도는 -180과 180 사이여야 합니다.");
+        }
+        if (latitude < -90 || latitude > 90) {
+            throw new IllegalArgumentException("위도는 -90과 90 사이여야 합니다.");
+        }
+    }
+}
